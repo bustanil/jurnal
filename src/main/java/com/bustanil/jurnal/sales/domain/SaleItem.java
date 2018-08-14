43dc702e9a9d37ecee7e6e25e0cccf690750d0f9
bustanil <bustanil.arifin@id.abb.com>
2018-08-14 17:11:46 +0800

    grid editor

--- src/main/java/com/bustanil/jurnal/sales/domain/SaleItem.java
+++ src/main/java/com/bustanil/jurnal/sales/domain/SaleItem.java
@@ -7,9 +7,10 @@ import lombok.RequiredArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@NoArgsConstructor(force = true)
-@Data
+//@NoArgsConstructor(force = true)
+//@Data
 @Entity
 public class SaleItem {
 
@@ -25,4 +26,62 @@ public class SaleItem {
         return price.multiply(BigDecimal.valueOf(quantity));
     }
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductCode() {
+        return productCode;
+    }
+
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SaleItem saleItem = (SaleItem) o;
+        return Objects.equals(id, saleItem.id);
+    }
+
+    @Override public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/bustanil/jurnal/sales/ui/SaleView.java
+++ src/main/java/com/bustanil/jurnal/sales/ui/SaleView.java
@@ -1,21 +1,44 @@
 package com.bustanil.jurnal.sales.ui;
 
 import com.bustanil.jurnal.sales.domain.SaleItem;
+import com.vaadin.data.Binder;
 import com.vaadin.navigator.View;
 import com.vaadin.spring.annotation.SpringView;
 import com.vaadin.ui.*;
+import com.vaadin.ui.components.grid.Editor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @SpringView(name = SaleView.VIEW_NAME)
 public class SaleView extends VerticalLayout implements View {
 
     public static final String VIEW_NAME = "sale";
 
+    private List<SaleItem> saleItems = new ArrayList<>();
+
     public SaleView(){
+        SaleItem firstRow = new SaleItem();
+        firstRow.setId(UUID.randomUUID().toString());
+        saleItems.add(firstRow);
+
         Grid<SaleItem> saleItemGrid = new Grid<>(SaleItem.class);
+        saleItemGrid.setItems(saleItems);
         saleItemGrid.setSizeFull();
         saleItemGrid.setColumnOrder("productCode", "productName", "quantity", "price", "subTotal");
         saleItemGrid.getColumn("id").setHidden(true);
         saleItemGrid.getColumn("productId").setHidden(true);
+
+        TextField productCodeField = new TextField();
+        Editor<SaleItem> editor = saleItemGrid.getEditor();
+        Binder<SaleItem> binder = editor.getBinder();
+        Binder.Binding<SaleItem, String> productCode1 = binder.bind(productCodeField, SaleItem::getProductCode, SaleItem::setProductCode);
+        editor.setEnabled(true);
+        editor.setBuffered(false);
+        Grid.Column<SaleItem, String> productCode = (Grid.Column<SaleItem, String>) saleItemGrid.getColumn("productCode");
+        productCode.setEditorBinding(productCode1);
+
         addComponent(saleItemGrid);
 
         HorizontalLayout bottomPart = new HorizontalLayout();
